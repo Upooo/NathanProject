1d1b5e414a4e8c267ff60d934f20a285dc754897
Upooo <aryasptnanggra@gmail.com>
2024-07-15 13:34:20 +0700

    update

---
+++ config.py
@@ -0,0 +1,128 @@
+import re
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+# Get this value from my.telegram.org/apps
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+# Get your mongo url from cloud.mongodb.com
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+DB_NAME = getenv("DB_NAME", "nathankece")
+
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 50000))
+
+# Chat id of a group for logging bot's activities
+LOGGER_ID = int(getenv("LOGGER_ID", None))
+
+# Get this value from @FallenxBot on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID", 7460160870))
+
+## Fill these variables if you're deploying on heroku.
+# Your heroku app name
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+# Get it from http://dashboard.heroku.com/account
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/Upooo/NathanProject",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+)  # Fill this variable if your upstream repository is private
+
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/nathanproject")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/nathansupport")
+
+# Set this to True if you want the assistant to automatically leave chats after an interval
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+
+
+# Get this credentials from https://developer.spotify.com/dashboard
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+
+
+# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 10000))
+
+
+# Telegram audio and video file size limit (in bytes)
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 123456789101112))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 123456789101112))
+# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
+
+
+# Get your pyrogram v2 session from @StringFatherBot on Telegram
+STRING1 = getenv("STRING_SESSION", None)
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+
+
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+
+
+"""
+https://telegra.ph//file/55db4316608dbe98d3971.jpg
+
+
+
+
+
+
+"""
+
+START_IMG_URL = getenv(
+    "START_IMG_URL", "https://telegra.ph//file/55db4316608dbe98d3971.jpg"
+)
+PING_IMG_URL = getenv(
+    "PING_IMG_URL", "https://telegra.ph//file/a0ed6c40134221e88d2e8.jpg"
+)
+STATS_IMG_URL = "https://telegra.ph//file/64f6f77ff39525973ac96.jpg"
+
+PLAYLIST_IMG_URL = "https://telegra.ph//file/043084295dce02e8a8d24.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph//file/043084295dce02e8a8d24.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph//file/043084295dce02e8a8d24.jpg"
+STREAM_IMG_URL = "https://telegra.ph//file/043084295dce02e8a8d24.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph//file/043084295dce02e8a8d24.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph//file/043084295dce02e8a8d24.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph//file/043084295dce02e8a8d24.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph//file/043084295dce02e8a8d24.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph//file/043084295dce02e8a8d24.jpg"
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
+
+if SUPPORT_CHANNEL:
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+        )
+
+if SUPPORT_CHAT:
+    if not re.match("(?:http|https)://", SUPPORT_CHAT):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
+        )
